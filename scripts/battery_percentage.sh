a53165fd71d2295f26dd7dd40967819439e49362
Sudhanshu <sudhansh@gmail.com>
2017-11-17 18:24:29 +0530

    Update battery_percentage.sh

--- scripts/battery_percentage.sh
+++ scripts/battery_percentage.sh
@@ -9,21 +9,11 @@ print_battery_percentage() {
 	if command_exists "pmset"; then
 		pmset -g batt | grep -o "[0-9]\{1,3\}%"
 	elif command_exists "upower"; then
-		local battery=$(upower -e | grep -m 1 battery)
-		if [ -z "$battery" ]; then
-			return
-		fi
-		local energy
-		local energy_full
-		energy=$(upower -i $battery | awk -v nrg="$energy" '/energy:/ {print nrg+$2}')
-		energy_full=$(upower -i $battery | awk -v nrgfull="$energy_full" '/energy-full:/ {print nrgfull+$2}')
-		if [ -n "$energy" ] && [ -n "$energy_full" ]; then
-			echo $energy $energy_full | awk '{printf("%d%%", ($1/$2)*100)}'
-		fi
+		battery=$(upower -e | grep battery)
+		percentage=$(upower -i $battery | awk '/percentage:/ {print $2}')
+		echo $percentage%
 	elif command_exists "acpi"; then
-		acpi -b | grep -m 1 -Eo "[0-9]+%"
-	elif command_exists "termux-battery-status"; then
-		termux-battery-status | jq -r '.percentage' | awk '{printf("%d%%", $1)}'
+		acpi -b | grep -Eo "[0-9]+%"
 	fi
 }
 
